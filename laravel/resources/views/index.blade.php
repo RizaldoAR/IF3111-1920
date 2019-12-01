124154f101d4526bbeeb0513842a9e7574a727b2
Muhammad Rizky Pratama <50003993+Tamagz@users.noreply.github.com>
2019-12-02 02:06:51 +0700

    Tampilan awal
    fitur search sama tampilan awal aja

---
+++ laravel/resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="css/cssutama.css" media="screen" rel="stylesheet">
+
+	<title></title>
+</head>
+<body>
+	<h1>SIMPLE LAPOR!</h1>
+	<div class="container">
+		<div class="box">
+	<form>
+  <input class="search" type="text" placeholder="Cari..." required>	
+  <input class="button" type="button" value="Cari">		
+</form>
+</div>
+</div>
+
+</body>
+</html>
