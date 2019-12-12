4de20b1d36b523cf7d75d6726dbd6efa485892af
Muhammad Rizky Pratama <50003993+Tamagz@users.noreply.github.com>
2019-12-13 03:33:37 +0700

    MENU UTAMA FIX
    fix menu utama

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -8,6 +8,7 @@
 <body>
 	<h1>SIMPLE LAPOR!</h1>
 	<div class="container">
+		
 		<div class="box">
 	<form>
   <input class="search" type="text" placeholder="Cari..." required>	
@@ -21,12 +22,64 @@
 </div>
 
 </div>
-
+<div class="recently">
 <p>Laporan/Komentar terakhir</p><br>
+
 <div class="garis">
-	<div class="text">
-		<p>jdfkla</p>
 </div>
+	<div class="text">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+</div>
+<div class="kumpulanlapor">
+	<div class="gambar">
+		<img src="views/bp.png" width="400">
+		<p>Waktu:13-12-2019 02.48 <a href="">Lihat selengkapnya>></a></p>
+	</div>
+	<div class="garislapor">
+		
+	</div>
+	<div class="text">
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+	</div>
+	<div class="kumpulanlapor">
+	<div class="gambar">
+		<img src="views/bp.png" width="400">
+		<p>Waktu:13-12-2019 03.19 <a href="">Lihat selengkapnya>></a></p>
+	</div>
+	<div class="garislapor">
+		
+	</div>
+	<div class="text">
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+	</div>
+	<div class="kumpulanlapor">
+	<div class="gambar">
+		<img src="views/bp.png" width="400">
+		<p>Waktu:13-12-2019 03.40 <a href="">Lihat selengkapnya>></a></p>
+	</div>
+	<div class="garislapor">
+		
+	</div>
+
+
 
 </body>
 </html>
