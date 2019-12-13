c5edfe9b141c22d0878f92b44996b0c9b51a871b
Muhammad Rizky Pratama <50003993+Tamagz@users.noreply.github.com>
2019-12-13 23:49:55 +0700

    PAGE BUAT LAPORAN
    pembuatan page buat laporan

---
+++ laravel/resources/views/buat.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/buat.css">
+	<title></title>
+</head>
+<body>
+	<div class="header">
+	<h1>SIMPLE LAPOR!</h1>
+	<p>Buat Laporan/Komentar</p>
+	<div class="garish"></div>
+	</div>
+	<div class="kotak">
+		
+		<form method="post" action="">
+<textarea name="alamat" rows="20" cols="140" placeholder="Laporan/Komentar"></textarea>
+	</div>
+	<div class="pilihan">
+		<p><select name="pilihan">
+			<option value='administrative'>Administrative</option>
+			<option value='infrastruktur'>Infrastruktur</option>
+		</select></p>
+	</div>
+
+
+<input type="button" name="buat" value="Buat laporan!">
+
+</body>
+</html>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('detail');
+    return view('buat');
 });
